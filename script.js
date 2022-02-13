a4d60ba0cb0efb2ec1e05638e0fee2c9a6defb7c
unknown <mnsdewari@gmail.com>
2022-02-13 15:29:41 +0530

    code completed

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 let text=prompt("enter message","");
 let count=parseInt(prompt("How many times???",10));
 window.InputEvent=window.Event || window.InputEvent;
+let event=new InputEvent("input",{bubbles:true});
+let textContainer=document.getElementsByClassName("_13NKt copyable-text selectable-text")[1];
+for(let i=0;i<count;i++)
+{
+    textContainer.innerHTML=text;
+    textContainer.dispatchEvent(event);
+    document.getElementsByClassName("_4sWnG")[0].click();
+}
+
+
+
+
+
+
